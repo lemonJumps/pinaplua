1bb663974ef21492523554e9a79ec3f5218112df
lemon jumps <lemonjumpsofficial@gmail.com>
2024-08-05 19:59:15 +0200

    closing on final design of the VM :D

--- pinaple/pineaple.c
+++ pinaple/pineaple.c
@@ -1,2 +1,14 @@
 #include "pineaple.h"
 
+/*
+
+how to work this thing:
+
+before the vm starts, register global things, variables, prebuilt classes.
+first put them into storage and then put their name into NameDB.
+
+if possible functions should be their own VMs, but could be just jumps;
+since the VM is literally just an I D and R stack. NOW the stacks can be interchanged on the fly/
+so, simple optimizations are possible
+
+*/
--- pinaple/pineaple.h
+++ pinaple/pineaple.h
@@ -20,7 +20,7 @@ Storage * last;
 
 
 size_t storage_addData(void * data, size_t size);
-void * storage_alloc(size_t size);
+void * storage_alloc(size_t size, size_t * id);
 void storage_delete(void * ptr);
 
 #pragma endregion
@@ -41,38 +41,40 @@ NameDB * last;
 } NameDB;
 
 void nameDB_addName(char * name, uint32_t id);
+void nameDB_rename(uint32_t id, char * newName);
+void nameDB_changeID(uint32_t oldId, uint32_t newId);
+void nameDB_changeID(char * name, uint32_t newId);
 void nameDB_removeName(char * name, uint32_t id);
 
 #pragma endregion
 
 #pragma region stack
 
-typedef enum
-{
-    instruction = 0,
-    data,
-    ret,
-    _count
-} ItemType;
-
 typedef struct
 {
 size_t reservedSize;
 
-size_t count;
+// size_t count;
 
 size_t iCount;
 size_t dCount;
 size_t rCount;
 
-uint32_t * id;
+uint32_t ** iStack;
+uint32_t ** dStack;
+uint32_t ** rStack;
 
-ItemType * type;
 
 Stack * next;
 Stack * last;
 } Stack;
 
+void stack_pushFN();
+void stack_pushData();
+void stack_pushReturn();
+void stack_pop();
+void stack_popFN();
+void stack_popFNandExecute();
 
 #pragma endregion
 
